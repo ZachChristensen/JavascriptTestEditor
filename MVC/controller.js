6d7c36fb41e0b94156aa4b623e9f57f78af4fb94
Planeyy <joh0784@student.cpit.ac.nz>
2016-09-24 11:52:17 +1200

    Added loadSuiteFromFile function

--- MVC/controller.js
+++ MVC/controller.js
@@ -8,4 +8,15 @@ class Controller{
 		this.myView = new View()
 		console.log("controller")
 	}
+	
+	loadSuiteFromFile(callback, inputElementId){
+		//takes a callback function to be executed after the suite has been loaded
+		//inputElementId expects a string
+    		let file = document.getElementById(inputElementId).files[0];
+    		let that = this;
+    		this.myModel.filer.loadSuiteFromFile(file, function(e) {
+      			that.createSuite(that.myModel.filer.splitString(e.target.result, "{"));
+      			callback();
+    		})
+  	}	
 }
