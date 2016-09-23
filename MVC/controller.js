549b7617db2130290276e3f080aff7f38de6f52e
Planeyy <joh0784@student.cpit.ac.nz>
2016-09-24 11:58:23 +1200

    Added createSuite function

--- MVC/controller.js
+++ MVC/controller.js
@@ -18,5 +18,23 @@ class Controller{
       			that.createSuite(that.myModel.filer.splitString(e.target.result, "{"));
       			callback();
     		})
-  	}	
+  	}
+  	
+  	createSuite(suiteArray){
+  		//takes a suite string that has been split on the { char
+    		let pattern = /(\}\))/i;
+    		let atRoot = true;
+    		for (let item of suiteArray){
+      			if (pattern.test(item)){
+        			let splitLine = item.split("})");
+        			this.createType(splitLine[0]);
+        			this.createType(splitLine[1]);
+      			}else if (atRoot){
+        			this.testFrameWork.root._description = this.getName(item);
+        			atRoot = false;
+      			}else{
+        			this.createType(item);
+      			}
+    		}
+  	}
 }
