362792979651e48fd62564d69d70eb3f3d0a269d
Planeyy <joh0784@student.cpit.ac.nz>
2016-09-24 11:47:09 +1200

    Added filer class

---
+++ MVC/filer.js
@@ -0,0 +1,14 @@
+class Filer{
+  loadSuiteFromFile(file, callback){
+    let suiteArray = [];
+    let result;
+    let that = this;
+    var reader = new FileReader();
+    reader.onload = callback;
+    reader.readAsText(file);
+  }
+
+  splitString(string, param){
+    return string.split(param);
+  }
+}
