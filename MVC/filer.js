23f033124e65b924fb071311229699ca544ed8b1
Planeyy <joh0784@student.cpit.ac.nz>
2016-09-24 12:11:27 +1200

    Removed semi colons

--- MVC/filer.js
+++ MVC/filer.js
@@ -1,14 +1,14 @@
 class Filer{
   loadSuiteFromFile(file, callback){
-    let suiteArray = [];
-    let result;
-    let that = this;
-    var reader = new FileReader();
-    reader.onload = callback;
-    reader.readAsText(file);
+    let suiteArray = []
+    let result
+    let that = this
+    var reader = new FileReader()
+    reader.onload = callback
+    reader.readAsText(file)
   }
 
   splitString(string, param){
-    return string.split(param);
+    return string.split(param)
   }
 }
