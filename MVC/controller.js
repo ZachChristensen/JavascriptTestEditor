0f19bbb4b44c987c7c3cd7f0a5131e7ab70ac294
Planeyy <joh0784@student.cpit.ac.nz>
2016-09-24 12:09:23 +1200

    Removed semi colons

--- MVC/controller.js
+++ MVC/controller.js
@@ -12,47 +12,47 @@ class Controller{
 	loadSuiteFromFile(callback, inputElementId){
 		//takes a callback function to be executed after the suite has been loaded
 		//inputElementId expects a string
-    		let file = document.getElementById(inputElementId).files[0];
-    		let that = this;
+    		let file = document.getElementById(inputElementId).files[0]
+    		let that = this
     		this.myModel.filer.loadSuiteFromFile(file, function(e) {
-      			that.createSuite(that.myModel.filer.splitString(e.target.result, "{"));
-      			callback();
+      			that.createSuite(that.myModel.filer.splitString(e.target.result, "{"))
+      			callback()
     		})
   	}
   	
   	createSuite(suiteArray){
   		//takes a suite string that has been split on the { char
-    		let pattern = /(\}\))/i;
-    		let atRoot = true;
+    		let pattern = /(\}\))/i
+    		let atRoot = true
     		for (let item of suiteArray){
       			if (pattern.test(item)){
-        			let splitLine = item.split("})");
-        			this.createType(splitLine[0]);
-        			this.createType(splitLine[1]);
+        			let splitLine = item.split("})")
+        			this.createType(splitLine[0])
+        			this.createType(splitLine[1])
       			}else if (atRoot){
-        			this.myModel.myTestFrameWork.root._description = this.getName(item);
+        			this.myModel.myTestFrameWork.root._description = this.getName(item)
         			atRoot = false;
       			}else{
-        			this.createType(item);
+        			this.createType(item)
       			}
     		}
   	}
   	
   	createType(suiteNodeString){
-    		suiteNodeString = suiteNodeString.trim();
+    		suiteNodeString = suiteNodeString.trim()
     		//regex is finding all letters from the start of the string
-    		let result = /^\w+/g.exec(suiteNodeString);
+    		let result = /^\w+/g.exec(suiteNodeString)
     		if (result == "describe"){
-      			let child = this.myModel.myTestFrameWork.addSuite(this.getName(suiteNodeString));
-      			this.myModel.myTestFrameWork.currentSuite = child;
+      			let child = this.myModel.myTestFrameWork.addSuite(this.getName(suiteNodeString))
+      			this.myModel.myTestFrameWork.currentSuite = child
     		}else if (result == "it"){
-      			this.myModel.myTestFrameWork.addSpec(this.getName(suiteNodeString));
+      			this.myModel.myTestFrameWork.addSpec(this.getName(suiteNodeString))
     		}
   	}
   	
   	getName(suiteNodeString){
   		//regex is searching for any characters between " " chars
-    		let name = /("[\s\S]+")/g.exec(suiteNodeString);
-    		return name[0].replace("\"",'');
+    		let name = /("[\s\S]+")/g.exec(suiteNodeString)
+    		return name[0].replace("\"",'')
   	}
 }
